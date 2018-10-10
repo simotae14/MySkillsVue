ff529bf72897bae2e878f60e9dee6b2a872e64f5
Simone Taeggi <simone.taeggi@leovegas.com>
2018-10-11 01:15:27 +0200

    Vue Forms: Added form, fields, submit handler and validation with VeeValidator

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="hello">
     <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input
+          type="text"
+          placeholder="Enter a skill you have..."
+          v-model="skill"
+          v-validate="'min: 5'"
+          name="skill"
+        >
+        <p
+          class="alert"
+          v-if="errors.has('skill')"
+        >
+          {{ errors.first('skill') }}
+        </p>
+      </form>
       <ul>
         <li
           v-for="(data, index) in skills" :key="index"
@@ -20,15 +35,25 @@ export default {
   name: 'Skills',
   data() {
     return {
+      skill: '',
       skills: [
           { "skill": "Vue.js" },
           { "skill": "Frontend Developer" }
-      ],
-      alertObject: {
-        bgColor: 'yellow',
-        bgWidth: '100%',
-        bgHeight: '30px'
-      }
+      ]
+    }
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          // add new skill
+          this.skills.push({ skill: this.skill });
+          // clear input field
+          this.skill = '';
+        } else {
+          console.log('Not valid value');
+        }
+      })
     }
   }
 }
@@ -63,5 +88,21 @@ export default {
 
   .container {
     box-shadow: 0px 0px 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VeeValidate from 'vee-validate'
 
+// to use VeeValidate
+Vue.use(VeeValidate);
 Vue.config.productionTip = false
 
 new Vue({
