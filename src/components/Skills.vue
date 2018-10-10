97f901b3d5c762b1629b1ad15bd334633501d742
Simone Taeggi <simone.taeggi@leovegas.com>
2018-10-10 23:30:50 +0200

    Vue Templates: adding directives and interpolation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="hello">
-    Skills
+    <div class="holder">
+      <ul>
+        <li
+          v-for="(data, index) in skills" :key="index"
+        >
+          {{ index }}. {{ data.skill }}
+        </li>
+      </ul>
+      <p v-if="skills.length > 1">
+        You have more than one skill
+      </p>
+      <p v-else>
+        You have less than or equal to 1 skill
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
-  props: {
+  data() {
+    return {
+      skills: [
+          { "skill": "Vue.js" },
+          { "skill": "Frontend Developer" }
+      ]
+    }
   }
 }
 </script>
